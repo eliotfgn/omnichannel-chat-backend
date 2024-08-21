1cc417650c6c339c0e831b2b19965ad17839608e
Eliot Fagnon <efagnon2002@gmail.com>
2024-08-21 08:59:53 +0100

    feat: Init middlewares in controller

--- core-service/src/app.ts
+++ core-service/src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import logger from './configs/logger';
+import errorMiddleware from '@middlewares/error.middleware';
 
 class App {
   private app: express.Application;
@@ -8,13 +9,25 @@ class App {
   constructor(port: number) {
     this.app = express();
     this.port = port;
+
+    this.initializePreRequestMiddlewares();
+    this.initializeControllers();
+    this.initializePostRequestMiddlewares();
   }
 
-  start = () => {
+  initializeControllers() {}
+
+  initializePreRequestMiddlewares() {}
+
+  initializePostRequestMiddlewares() {
+    this.app.use(errorMiddleware);
+  }
+
+  start() {
     this.app.listen(this.port, () => {
       logger.info(`Application started on port ${this.port}.`);
     });
-  };
+  }
 }
 
 export default App;
